e46a7453745dc7a292cd4fa088b769b71b1f061b
Dzmitry.Yaniuk <Dzmitry.Yaniuk@coxautoinc.com>
2021-04-11 18:37:01 +0300

    Fix missing projectId in team modal

--- web/src/components/modal/team/TeamModalContainer.tsx
+++ web/src/components/modal/team/TeamModalContainer.tsx
@@ -2,21 +2,28 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { BaseRegexExpression } from '../../../constants';
 import * as teamActions from '../../../redux/actions/teamActions';
+import { getSelectProjectId } from '../../../redux/selectors/projectSelectors';
 import * as projectSelectors from '../../../redux/selectors/projectSelectors';
 import { ITeam } from '../../../types/teamTypes';
 import { InputFormFieldValidator } from '../../../utils/formHelper';
 import TeamModal, { ITeamModalProps } from './TeamModal';
 
-const TeamContainerModal = () => {
+const TeamModalContainer = () => {
     const dispatch = useDispatch();
 
+    const projectId: string = useSelector(getSelectProjectId);
     const projects = useSelector(projectSelectors.getProjectNames);
 
     const validateField = (value: string): string =>
         new InputFormFieldValidator(value, 1, 100, true, BaseRegexExpression).validate();
 
     const onSubmit = (values: ITeam) => {
-        dispatch(teamActions.createTeamRequest(values));
+        const team: ITeam = {
+            ...values,
+            projectId,
+        };
+
+        dispatch(teamActions.createTeamRequest(team));
     };
 
     const teamCreationProps: ITeamModalProps = {
@@ -28,4 +35,4 @@ const TeamContainerModal = () => {
     return <TeamModal {...teamCreationProps} />;
 };
 
-export default TeamContainerModal;
+export default TeamModalContainer;
