d3595f366af3e9ca7908b21d74301b07461f853c
Dmitry Yaniuk <Dzmitry.Yaniuk@coxautoinc.com>
2021-05-02 18:29:36 +0300

    Update story endpoints description

--- WebAPI/WebAPI/Presentation/Controllers/StoryController.cs
+++ WebAPI/WebAPI/Presentation/Controllers/StoryController.cs
@@ -38,8 +38,8 @@ namespace WebAPI.Presentation.Controllers
         [Route("all")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        public async Task<ActionResult<CollectionResponse<Story>>> GetAllStories()
-            => await _storyService.GetStoriesAsync();
+        public async Task<ActionResult<CollectionResponse<Story>>> GetAllStories() =>
+            await _storyService.GetStoriesAsync();
 
         /// <summary>
         /// Sort stories in particular order from epic by params criteria
@@ -57,25 +57,29 @@ namespace WebAPI.Presentation.Controllers
             [FromQuery, BindRequired] Guid[] storyIds,
             [FromQuery, BindRequired] string sortType,
             [FromQuery, BindRequired] OrderType orderType
-            )
-        {
-            var sortedStories = await _storyService.SortStories(
-                storyIds,
-                sortType,
-                orderType
-            );
-
-            return sortedStories;
-        }
-
+            ) => 
+                await _storyService.SortStories(storyIds, sortType, orderType);
+        
+        /// <summary>
+        /// Receive all stories from epic by epic id
+        /// </summary>
+        /// <response code="200">Receiving all stories from epic by epic id</response>
+        /// <response code="401">Failed authentication</response>
+        /// <response code="404">Unable to find any stories with provided epic id</response>
         [HttpGet]
         [Route("epic/id/{epicId}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<CollectionResponse<Story>>> GetStoriesFormEpic(Guid epicId) => 
             await _storyService.GetStoriesFromEpicAsync(epicId);
         
-        
+        /// <summary>
+        /// Receive all stories from epic by sprint id
+        /// </summary>
+        /// <response code="200">Receiving all stories from epic by sprint id</response>
+        /// <response code="401">Failed authentication</response>
+        /// <response code="404">Unable to find any stories with provided sprint id</response>
         [HttpGet]
         [Route("sprint/id/{sprintId}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
@@ -136,13 +140,9 @@ namespace WebAPI.Presentation.Controllers
         [HttpPut]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        public async Task<ActionResult<Story>> UpdateStory([FromBody, BindRequired] Story story)
-        {
-            var updatedStory = await _storyService.UpdateStoryAsync(story);
+        public async Task<ActionResult<Story>> UpdateStory([FromBody, BindRequired] Story story) => 
+            await _storyService.UpdateStoryAsync(story);
 
-            return updatedStory;
-        }
-        
         /// <summary>
         /// Update story and write updates to story history with provided model properties
         /// </summary>
