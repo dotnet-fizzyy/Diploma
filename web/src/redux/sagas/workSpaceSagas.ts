06d47a2215e92fc11aa4e7fc24afda486ea9a01d
Dmitry Yaniuk <Dzmitry.Yaniuk@coxautoinc.com>
2021-05-15 23:06:09 +0300

    Update workspace create saga to push user on it's page after creating

--- web/src/redux/sagas/workSpaceSagas.ts
+++ web/src/redux/sagas/workSpaceSagas.ts
@@ -1,40 +1,52 @@
-import { call, put, takeLatest } from 'redux-saga/effects';
+import { push } from 'connected-react-router';
+import { all, call, put, takeLatest } from 'redux-saga/effects';
 import WorkSpaceApi from '../../api/workSpaceApi';
+import { WorkspaceViewerRoute } from '../../constants/routeConstants';
 import { IWorkSpace, IWorkSpacePage } from '../../types/workSpaceTypes';
-import * as WorkSpaceActions from '../actions/workSpaceActions';
+import {
+    createWorkSpaceFailure,
+    createWorkSpaceSuccess,
+    getUserWorkSpacePageFailure,
+    getUserWorkSpacePageSuccess,
+    updateWorkSpaceError,
+    updateWorkSpaceSuccess,
+    ICreateWorkSpaceRequest,
+    IUpdateWorkSpaceSuccess,
+    WorkSpaceActions,
+} from '../actions/workSpaceActions';
 
 function* getUserWorkSpacePage() {
     try {
-        const createdWorkSpace: IWorkSpacePage = yield call(WorkSpaceApi.getUserWorkSpace);
+        const workSpace: IWorkSpacePage = yield call(WorkSpaceApi.getUserWorkSpace);
 
-        yield put(WorkSpaceActions.getUserWorkSpacePageSuccess(createdWorkSpace));
+        yield put(getUserWorkSpacePageSuccess(workSpace));
     } catch (error) {
-        yield put(WorkSpaceActions.getUserWorkSpacePageFailure(error));
+        yield put(getUserWorkSpacePageFailure(error));
     }
 }
 
-function* createWorkSpace(action: WorkSpaceActions.ICreateWorkSpaceRequest) {
+function* createWorkSpace(action: ICreateWorkSpaceRequest) {
     try {
         const createdWorkSpace: IWorkSpace = yield call(WorkSpaceApi.createWorkSpace, action.payload);
 
-        yield put(WorkSpaceActions.createWorkSpaceSuccess(createdWorkSpace));
+        yield all([put(createWorkSpaceSuccess(createdWorkSpace)), push(WorkspaceViewerRoute)]);
     } catch (error) {
-        yield put(WorkSpaceActions.createWorkSpaceFailure(error));
+        yield put(createWorkSpaceFailure(error));
     }
 }
 
-function* updateWorkSpace(action: WorkSpaceActions.IUpdateWorkSpaceSuccess) {
+function* updateWorkSpace(action: IUpdateWorkSpaceSuccess) {
     try {
-        const createdWorkSpace: IWorkSpace = yield call(WorkSpaceApi.updateWorkSpace, action.payload);
+        const updatedWorkSpace: IWorkSpace = yield call(WorkSpaceApi.updateWorkSpace, action.payload);
 
-        yield put(WorkSpaceActions.updateWorkSpaceSuccess(createdWorkSpace));
+        yield put(updateWorkSpaceSuccess(updatedWorkSpace));
     } catch (error) {
-        yield put(WorkSpaceActions.updateWorkSpaceError(error));
+        yield put(updateWorkSpaceError(error));
     }
 }
 
 export default function* workSpaceSaga() {
-    yield takeLatest(WorkSpaceActions.WorkSpaceActions.GET_USER_WORKSPACE_PAGE_REQUEST, getUserWorkSpacePage);
-    yield takeLatest(WorkSpaceActions.WorkSpaceActions.CREATE_WORKSPACE_REQUEST, createWorkSpace);
-    yield takeLatest(WorkSpaceActions.WorkSpaceActions.UPDATE_WORKSPACE_REQUEST, updateWorkSpace);
+    yield takeLatest(WorkSpaceActions.GET_USER_WORKSPACE_PAGE_REQUEST, getUserWorkSpacePage);
+    yield takeLatest(WorkSpaceActions.CREATE_WORKSPACE_REQUEST, createWorkSpace);
+    yield takeLatest(WorkSpaceActions.UPDATE_WORKSPACE_REQUEST, updateWorkSpace);
 }
