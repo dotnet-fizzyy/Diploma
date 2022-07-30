6493cc6ad7610bf23e0f8dda65c00f1143d17205
Dmitry Yaniuk <ezzyfizzy27@gmail.com>
2022-07-31 00:20:56 +0300

    Update SprintController actions XML description

--- WebAPI/WebAPI/Presentation/Controllers/SprintController.cs
+++ WebAPI/WebAPI/Presentation/Controllers/SprintController.cs
@@ -23,64 +23,62 @@ namespace WebAPI.Presentation.Controllers
         }
         
         /// <summary>
-        /// Receive all sprints with description from epic by provided epic id
+        /// Gets sprints full description by provided epic and team ids.
         /// </summary>
-        /// <response code="200">Receiving all sprints from epic by provided epic id</response>
-        /// <response code="401">Failed authentication</response>
-        /// <response code="404">Unable to find sprints with description by provided epic id</response>
+        /// <param name="epicId">Epic identifier.</param>
+        /// <param name="teamId">Epic identifier.</param>
+        /// <response code="200">A collection of sprints by provided epic and team ids.</response>
+        /// <response code="401">Failed authentication.</response>
+        /// <response code="404">Unable to find sprints by provided epic id and team ids.</response>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         [HttpGet]
         [Route("epic/id/{epicId:guid}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<CollectionResponse<FullSprint>>> GetAllSprintsFromEpic(Guid epicId, [FromQuery]Guid? teamId)
-        {
-            var boardResponse = await _sprintService.GetAllSprintsFromEpicAsync(epicId, teamId);
-
-            return boardResponse;
-        }
+        public async Task<ActionResult<CollectionResponse<FullSprint>>> GetAllSprintsFromEpic(
+            Guid epicId, 
+            [FromQuery] Guid? teamId) => 
+                await _sprintService.GetAllSprintsFromEpicAsync(epicId, teamId);
 
         /// <summary>
-        /// Receive sprint by provided id
+        /// Gets sprint by provided id.
         /// </summary>
-        /// <response code="200">Receiving sprint by provided id</response>
-        /// <response code="401">Failed authentication</response>
-        /// <response code="404">Unable to find sprint by provided id</response>
+        /// <param name="id">Sprint identifier.</param>
+        /// <response code="200">Sprint by provided id.</response>
+        /// <response code="401">Failed authentication.</response>
+        /// <response code="404">Unable to find sprint by provided id.</response>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         [HttpGet]
         [Route("id/{id:guid}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<Sprint>> GetSprint(Guid id)
-        {
-            var sprint = await _sprintService.GetByIdAsync(id);
+        public async Task<ActionResult<Sprint>> GetSprint(Guid id) =>
+            await _sprintService.GetByIdAsync(id);
 
-            return sprint;
-        }
-        
         /// <summary>
-        /// Receive sprint with full description by provided id
+        /// Gets sprint full description by provided id.
         /// </summary>
-        /// <response code="200">Receiving sprint with full description by provided id</response>
-        /// <response code="401">Failed authentication</response>
-        /// <response code="404">Unable to find sprint with full description by provided id</response>
+        /// <param name="id">Sprint identifier.</param>
+        /// <response code="200">Sprint full description by provided id.</response>
+        /// <response code="401">Failed authentication.</response>
+        /// <response code="404">Unable to find sprint by provided id.</response>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         [HttpGet]
         [Route("full/id/{id:guid}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<FullSprint>> GetFullSprint(Guid id)
-        {
-            var fullSprint =  await _sprintService.GetFullSprintAsync(id);
-
-            return fullSprint;
-        }
+        public async Task<ActionResult<FullSprint>> GetFullSprint(Guid id) =>
+            await _sprintService.GetFullSprintAsync(id);
 
         /// <summary>
-        /// Create sprint with provided model properties
+        /// Creates sprint.
         /// </summary>
-        /// <response code="201">Created sprint with provided model properties</response>
-        /// <response code="401">Failed authentication</response>
+        /// <response code="201">Created sprint.</response>
+        /// <response code="401">Failed authentication.</response>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
@@ -92,30 +90,30 @@ namespace WebAPI.Presentation.Controllers
         }
         
         /// <summary>
-        /// Update sprint with provided model properties
+        /// Updates sprint.
         /// </summary>
-        /// <response code="200">Updated sprint with provided model properties</response>
-        /// <response code="401">Failed authentication</response>
+        /// <param name="sprint"><see cref="Sprint"/> model</param>
+        /// <response code="200">Updated sprint.</response>
+        /// <response code="401">Failed authentication.</response>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         [HttpPut]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        public async Task<IActionResult> UpdateSprint([FromBody, BindRequired] Sprint sprint)
-        {
-            var updatedSprint = await _sprintService.UpdateAsync(sprint);
+        public async Task<ActionResult<Sprint>> UpdateSprint([FromBody, BindRequired] Sprint sprint) =>
+            await _sprintService.UpdateAsync(sprint);
 
-            return Ok(updatedSprint);
-        }
-        
         /// <summary>
-        /// Soft remove sprint by sprintId
+        /// Updates sprint deleted status by provided id.
         /// </summary>
-        /// <response code="204">Successful soft remove sprint by sprintId</response>
-        /// <response code="401">Failed authentication</response>
+        /// <param name="id">Sprint identifier.</param>
+        /// <response code="204">Sprint deleted status was set.</response>
+        /// <response code="401">Failed authentication.</response>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         [HttpDelete]
         [Route("soft-remove/id/{id:guid}")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        public async Task<IActionResult> SprintSoftRemove(Guid id)
+        public async Task<ActionResult> SprintSoftRemove(Guid id)
         {
             await _sprintService.SoftRemoveAsync(id);
             
@@ -123,15 +121,17 @@ namespace WebAPI.Presentation.Controllers
         }
         
         /// <summary>
-        /// Remove sprint with provided id
+        /// Removes sprint from DB by provided id.
         /// </summary>
-        /// <response code="204">Removed sprint with provided id</response>
-        /// <response code="401">Failed authentication</response>
+        /// <param name="id">Sprint identifier.</param>
+        /// <response code="204">Sprint was removed from DB.</response>
+        /// <response code="401">Failed authentication.</response>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         [HttpDelete]
         [Route("remove/id/{id:guid}")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        public async Task<IActionResult> RemoveSprint(Guid id)
+        public async Task<ActionResult> RemoveSprint(Guid id)
         {
             await _sprintService.RemoveAsync(id);
 
